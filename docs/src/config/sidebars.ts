db18aaed358685bc767ed2df0d3859978a9f688b
mkm17 <kornetmichal@gmail.com>
2024-10-30 22:07:21 +0100

    Adds spp autofillcolumn apply command. Closes #6203

--- docs/src/config/sidebars.ts
+++ docs/src/config/sidebars.ts
@@ -4103,6 +4103,15 @@ const sidebars: SidebarsConfig = {
     {
       'SharePoint Premium (spp)': [
         {
+          autofillcolumn: [
+            {
+              type: 'doc',
+              label: 'autofillcolumn set',
+              id: 'cmd/spp/autofillcolumn/autofillcolumn-set'
+            }
+          ]
+        },
+        {
           contentcenter: [
             {
               type: 'doc',
